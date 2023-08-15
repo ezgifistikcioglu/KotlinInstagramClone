6d2c77e8e15599e9aadb69c4b6c6ecf649f7ba30
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2023-08-15 19:03:03 +0300

    Added logout and menu.

--- app/src/main/java/com/ezgieren/kotlininstagramclone/FeedActivity.kt
+++ app/src/main/java/com/ezgieren/kotlininstagramclone/FeedActivity.kt
@@ -1,14 +1,44 @@
 package com.ezgieren.kotlininstagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.ezgieren.kotlininstagramclone.databinding.ActivityFeedBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFeedBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.instagram_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_post) {
+            val intent = Intent(this, UploadActivity::class.java)
+            startActivity(intent)
+        } else if (item.itemId == R.id.signOut) {
+            auth.signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
