7b00c92bc3a8ca4a352ed6b23b802e0b7a6766cd
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2023-08-15 15:32:39 +0300

    Revert "Added basic ui and firebase"
    This reverts commit a6aef1f11a89e98801458db3ffa358f8c94aa7e6.

--- app/src/main/java/com/ezgieren/kotlininstagramclone/MainActivity.kt
+++ app/src/main/java/com/ezgieren/kotlininstagramclone/MainActivity.kt
@@ -3,15 +3,11 @@ package com.ezgieren.kotlininstagramclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.ezgieren.kotlininstagramclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
     fun signInClicked(view: View?) {
     }
